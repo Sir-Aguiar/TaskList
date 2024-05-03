bd43d9b7bf1703bee7fd2736afad88a21009259d
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-05-03 01:14:07 -0400

    feat: Adding task

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,34 +1,34 @@
 package entities;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class Task {
-  public final UUID taskId;
+  public final String taskId;
   private final String taskTitle;
-  private Date taskStart;
-  private Date taskEnd;
+  private LocalDate taskStart;
+  private LocalDate taskEnd;
 
-  public Task(String taskTitle, Date taskStart, Date taskEnd) {
+  public Task(String taskTitle, LocalDate taskStart, LocalDate taskEnd) {
     this.taskTitle = taskTitle;
     this.taskStart = taskStart;
     this.taskEnd = taskEnd;
-    this.taskId = UUID.randomUUID();
+    this.taskId = UUID.randomUUID().toString();
   }
 
-  public void updateTaskEnd(Date taskEnd) {
+  public void updateTaskEnd(LocalDate taskEnd) {
     this.taskEnd = taskEnd;
   }
 
-  public void updateTaskStart(Date taskStart) {
+  public void updateTaskStart(LocalDate taskStart) {
     this.taskStart = taskStart;
   }
 
-  public Date getTaskEnd() {
+  public LocalDate getTaskEnd() {
     return this.taskEnd;
   }
 
-  public Date getTaskStart() {
+  public LocalDate getTaskStart() {
     return this.taskStart;
   }
 
--- src/entities/TaskList.java
+++ src/entities/TaskList.java
@@ -1,38 +1,38 @@
 package entities;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.UUID;
+import java.util.Objects;
 
 public class TaskList {
 
+  public ArrayList<Task> tasks;
   String listOwner;
-  ArrayList<Task> tasks;
 
   public TaskList(String listOwner) {
     this.listOwner = listOwner;
-
-    System.out.printf("[SUCCESS] Task list created successfully by %S", listOwner);
+    this.tasks = new ArrayList<>();
+    System.out.printf("[SUCCESS] Task list created successfully by %S \n", listOwner);
   }
 
 
-  public UUID addTask(String taskTitle, Date taskStart, Date taskEnd) {
+  public String addTask(String taskTitle, LocalDate taskStart, LocalDate taskEnd) {
     Task createdTask = new Task(taskTitle, taskStart, taskEnd);
     this.tasks.add(createdTask);
-    System.out.printf("[SUCCESS] '%S' has been successfully added", taskTitle);
+    System.out.printf("[SUCCESS] '%S' has been successfully added \n", taskTitle);
     return createdTask.taskId;
   }
 
-  public void removeTask(UUID taskId) {
+  public void removeTask(String taskId) {
     Iterator<Task> iterator = this.tasks.iterator();
 
     while (iterator.hasNext()) {
       Task task = iterator.next();
 
-      if (task.taskId.equals(taskId)) {
+      if (Objects.equals(task.taskId, taskId)) {
         iterator.remove();
-        System.out.println("[SUCCESS] Specified task has been successfully removed");
+        System.out.println("[SUCCESS] Specified task has been successfully removed \n");
 
         break;
       }
